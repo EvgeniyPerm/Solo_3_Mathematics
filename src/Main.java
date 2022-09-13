3a346c6a5b5edbde0ec788a709398731fc78f78d
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-14 02:05:10 +0500

    Find which math expression matches the answer that you are given, if you have an integer answer, and a list of math expressions.
    Task:
    Test each math expression to find the first one that matches the answer that you are given.
    
    Input Format:
    Two inputs: an integer and a space separated string of math expressions. The following operations need to be supported: addition +, subtraction -, multiplication *, division /.
    An expression can include multiple operations.
    
    Output Format:
    A string that tells the index of the first math expression that matches. If there are no matches, output 'none'.
    
    Sample Input:
    15
    (2+100) (5*3) (14+1)
    
    Sample Output:
    index 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static int countTwo(int a, int b, char sign){
+        switch (sign){
+            case '+': return a+b;
+            case '-': return a-b;
+            case '*': return a*b;
+            case '/': return a/b;
+        }
+        return 0;
+    }
+
+    public static int countExp(String exp){
+         String expClear = exp.replace("+"," ")
+                                .replace("-"," ")
+                                 .replace("*"," ")
+                                 .replace("/"," ");
+        String[] numbers= expClear.split(" ");
+        int position=numbers[0].length();
+        int sum=Integer.parseInt(numbers[0]);
+        for (int i=0;i<numbers.length-1;i++){
+            sum=countTwo(sum,Integer.parseInt(numbers[i+1]),exp.charAt(position));
+            position+=numbers[i+1].length()+1;
+        };
+        return sum;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int answer = Integer.parseInt(scanner.nextLine());
+
+        String[] exps = scanner.nextLine().replace("(","").replace(")","").split(" ");
+        int i=0;
+        for (String exp:exps) {
+            if (countExp(exp)==answer) {
+                System.out.println("index "+i);
+                return;
+            }
+            i++;
+        }
+        if (i==exps.length) System.out.println("none");
     }
 }
